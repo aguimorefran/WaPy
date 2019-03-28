980624a039e626ab01ec2e34cc8dee7f4a5cce8a
Francisco Aguilera <aguimorefran@gmail.com>
2019-03-28 15:17:18 +0100

    Add files via upload

--- WaPy/waPy.py
+++ WaPy/waPy.py
@@ -2,6 +2,7 @@ import datetime
 #   waPy.py
 #   Takes a txt WhatsApp converastion and displays relevant data
 
+#   Message class
 class Message:
     #   08/03/2019, 17:19 - Fco: Hi
     line = ""
@@ -13,27 +14,36 @@ class Message:
         #   first check if the line is a message
         if input != '\n' and len(input) > 0 and (input[2] == '/' and input[5] == '/'):
             self.line = input.rstrip()
-            day = int(line[0:2])
-            month = int(line[3:5])
-            year = int(line[6:10])
-            hour = int(line[12:14])
-            minute = int(line[15:17])
+            day = int(self.line[0:2])
+            month = int(self.line[3:5])
+            year = int(self.line[6:10])
+            hour = int(self.line[12:14])
+            minute = int(self.line[15:17])
             self.time = datetime.datetime(year, month, day, hour, minute)
-            self.author = line[20:].split(':')[0]
+            self.author = self.line[20:].split(':')[0]
             # find second occurrence of ':'
-            
 
+#   Reads from file 'filepath'
+def readFromFile(filepath):
+    with open(filepath, encoding="utf8") as fp:
+        line = fp.readline()
+        while line:
+            msg = Message()
+            msg.processMessage(line)
+            if(msg.line != ''):
+                msgList.append(msg)
+            line = fp.readline()
+
+            
+filepath = "WaPy/conver1.txt"
 msgList = []
 authorList = []
 
-
 #   read the file line by line, create the messages and store them in msgList
-filepath = "WaPy/conver1.txt"
-with open(filepath, encoding="utf8") as fp:
-    line = fp.readline()
-    while line:
-        msg = Message()
-        msg.processMessage(line)
-        if(msg.line != ''):
-            msgList.append(msg)
-        line = fp.readline()
+readFromFile(filepath)
+
+#   get a list of the authors in the conversation
+for msg in msgList:
+    if msg.author not in authorList:
+        authorList.append(msg.author)
+
