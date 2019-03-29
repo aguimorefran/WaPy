15005e8352b4a27fc15fcf9a642899aee7d3f371
Francisco Aguilera <aguimorefran@gmail.com>
2019-03-29 12:22:41 +0100

    Start all over again. Major changes
    Didn't know the __init__ thing while creating a class in Py, so there where a lot of failures. Now redefining the entire structure of the project.

--- WaPy/waPy.py
+++ WaPy/waPy.py
@@ -1,69 +1,27 @@
 import datetime
-#   waPy.py
-#   Takes a txt WhatsApp converastion and displays relevant data
-
-#   Message class
-
 
 class Message:
-    #   08/03/2019, 17:19 - Fco: Hi
-    line = ""
-    time = datetime
-    author = ""
-    content = ""
+    def __init__(self, line, user, content, year, month, day, hour, minute):
+        self.line = line
+        self.user = user
+        self.content = content
+        self.time = datetime.datetime(year, month, day, hour, minute)
+    def toString(self):
+        return ("%s\n%s\n%s" %(self.user, self.content, self.time))
 
-    def processMessage(self, input):
-        #   first check if the line is a message
-        if input != '\n' and len(input) > 0 and (input[2] == '/' and input[5] == '/'):
-            self.line = input.rstrip()
-            day = int(self.line[0:2])
-            month = int(self.line[3:5])
-            year = int(self.line[6:10])
-            hour = int(self.line[12:14])
-            minute = int(self.line[15:17])
-            self.time = datetime.datetime(year, month, day, hour, minute)
-            self.author = self.line[20:].split(':')[0]
-            # find second occurrence of ':'
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.msgs = []
+        self.files = []
+    def toString(self):
+        return ("Name: %s, msgs: %d, files: %d" %(self.name, len(self.msgs), len(self.files)))
 
-#   Reads from file 'filepath'
+def addUser(username, userList):
+    ok = False
+    for u in userList:
+        if username == u.name:
+            ok = True
+    if ok is False:
+        userList.append(User(username))
 
-
-def readFromFile(filepath):
-    with open(filepath, encoding="utf8") as fp:
-        line = fp.readline()
-        while line:
-            msg = Message()
-            msg.processMessage(line)
-            if(msg.line != ''):
-                msgList.append(msg)
-            line = fp.readline()
-
-class Author:
-    name = ""
-    nMsgs = 0
-    nFiles = 0
-    
-def createAuthors():
-    for msg in msgList:
-        ok = False
-        for au in authorList:
-            if au.name == msg.author:
-                ok = True
-        if not ok:
-            a = Author()
-            a.name = msg.author
-            authorList.append(a)
-
-def assignMsgs():
-    # read msg list and add msgs to the Author.nMsg
-
-filepath = "WaPy/conver1.txt"
-msgList = []
-authorList = []
-msgPerAuthor = {}
-
-#   read the file line by line, create the messages and store them in msgList
-readFromFile(filepath)
-
-#   create list of authors
-createAuthors()
