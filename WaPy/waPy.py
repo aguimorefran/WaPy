8e86556ba6031ef42508893eb19ae5a7240e249e
aguimorefran <aguimorefran@gmail.com>
2019-10-29 00:21:21 +0100

    Great changes. Starting almost again.

--- WaPy/waPy.py
+++ WaPy/waPy.py
@@ -3,40 +3,19 @@ import string
 
 
 class Message:
-    def __init__(self, line, user, content, year, month, day, hour, minute):
+
+    def __init__(self, user, line, content, time):
         self.line = line
         self.user = user
         self.content = content
-        self.time = datetime.datetime(year, month, day, hour, minute)
+        self.time = time
 
-    def toString(self):
-        return ("'%s'->'%s'\n'%s'" % (self.user, self.content, self.time))
-
-
-class User:
-    def __init__(self, name):
-        self.name = name
-        self.msgs = []
-        self.files = []
-
-    def toString(self):
-        return ("Name: %s, msgs: %d, files: %d" % (self.name, len(self.msgs), len(self.files)))
-
-#   Checks if user exists. If not, create and add it
-
-
-def addUser(username, userList):
-    ok = False
-    for u in userList:
-        if username == u.name:
-            ok = True
-    if ok is False:
-        userList.append(User(username))
 
 #   Parses a line of message into its different fields
-
-
 def parseMsg(input):
+    # 012345678901234567890123456789
+    # 10/10/2018, 19:33 - Lau: hola que tal
+    # 10/10/2018, 19:34 - Fco: bien y tu
     line = input.rstrip()
     day = int(line[0:2])
     month = int(line[3:5])
@@ -45,141 +24,22 @@ def parseMsg(input):
     minute = int(line[15:17])
     content = line[20:].split(':')[1]
     user = line[20:].split(':')[0]
-    return Message(line, user, content[1:], year, month, day, hour, minute)
+    time = datetime.datetime(year, month, day, hour, minute)
+    return Message(user, line, content, time)
 
 #   read conversation file and create a list with its parsed msgs
-
-
-def readFromFile(filepath, msgList):
-    with open(filepath, encoding="utf8") as fp:
+def readFromFile(filepath):
+    msgList = []
+    with open(filepath, encoding="utf-8") as fp:
         line = fp.readline()
         while line:
-            if len(line) > 0 and line != "\n":
-                msgList.append(parseMsg(line))
+            if len(line) > 0 and line != "\n" and line.find("Messages to this chat and calls") == -1:
+                # parse line to message
+                msg = parseMsg(line)
+                msgList.append(msg)
             line = fp.readline()
+    fp.close()
+    return msgList
 
-#   creates a list of users from the msgList
-
-
-def createUsers(msgList, userList):
-    for msg in msgList:
-        ok = False
-        for u in userList:
-            if u.name == msg.user:
-                ok = True
-        if not ok:
-            userList.append(User(msg.user))
-
-#   assigns every msg of msgList to a user
-
-
-def assignMsgs(msgList, userList):
-    for msg in msgList:
-        for u in userList:
-            if msg.user == u.name:
-                if "omitted" in msg.content:
-                    u.files.append(msg)
-                else:
-                    u.msgs.append(msg)
-#   Takes a user. msg = True -> displays msgs list data, msg = False -> displays files list data
-#   avg = True -> divides data by total num of msgs
-
-
-def msgPerHour(user, msg, avg):
-    hours = [0]*24
-    if msg is True:
-        for msg in user.msgs:
-            hours[msg.time.hour] += 1
-        if avg is True:
-            return [x / len(user.msgs) for x in hours]
-        else:
-            return hours
-    else:
-        for fi in user.files:
-            hours[fi.time.hour] += 1
-        if avg is True:
-            return [x / len(user.files) for x in hours]
-        else:
-            return hours
-
-
-def msgPerDOW(user, msg, avg):
-    DOW = [0]*7
-    if msg is True:
-        for msg in user.msgs:
-            DOW[msg.time.weekday()] += 1
-        if avg is True:
-            return[x / len(user.msgs) for x in DOW]
-        else:
-            return DOW
-    else:
-        for msg in user.files:
-            DOW[msg.time.weekday()] += 1
-        if avg is True:
-            return[x / len(user.files) for x in DOW]
-        else:
-            return DOW
-
-
-def mostActiveDay(user):
-    mad = {}
-    max = 0
-    acu = 0
-    for i in range(len(user.msgs)-1):
-        if user.msgs[i+1].time.day == user.msgs[i].time.day and user.msgs[i+1].time.month == user.msgs[i].time.month:
-            acu = acu + 1
-        elif acu > max:
-            max = acu
-            acu = 0
-    mad[acu] = user.msgs[i].time.strftime('%d-%m-%Y')
-    
-    acu = 0
-    for i in range(len(user.msgs)-1):
-        if user.msgs[i+1].time.day == user.msgs[i].time.day and user.msgs[i+1].time.month == user.msgs[i].time.month:
-            acu = acu + 1
-        elif acu == max:
-            mad[acu] = user.msgs[i].time.strftime('%d-%m-%Y')
-
-    return mad
-
-def avgWordsPerMsg(user):
-    avg = 0
-    for msg in user.msgs:
-        avg += len(msg.content.split())
-    return avg/len(user.msgs)
-
-def getRawWords(msgList, rawWordList):
-    for msg in msgList:
-        for w in msg.content.split():
-            w = w.lower().strip(",.!¡?¿;:-_")
-            if w not in rawWordList:
-                rawWordList.append(w)
-    rawWordList.sort()
-
-def createAlphabet(alphaPath, alphabet):
-    with open(alphaPath, encoding="utf8") as fp:
-        line = fp.readline()
-        while line:
-            alphabet.append(line)
-            line = fp.readline()
-    alphabet.sort()
-
-def createDict(alphabet, dictfilepath):
-
-
-
-filepath = "WaPy/elena.txt"
-alphaPath = "WaPy/alfa.txt"
-dictfilepath = "WaPy/dict.txt"
-alphabet = []
-msgList = []
-userList = []
-rawWordList = []
-niceDict = []
-
-readFromFile(filepath, msgList)
-createUsers(msgList, userList)
-assignMsgs(msgList, userList)
-getRawWords(msgList, rawWordList)
-createAlphabet(alphaPath, alphabet)
-fixWordInput(rawWordList, niceDict)
+#TODO: create user class with msg/media per hour and DOW
+#TODO: tener en cuenta dejarlo todo organizado en clases
