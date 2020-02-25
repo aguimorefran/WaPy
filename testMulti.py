0023dc3addba3daf477aff816b57ae6c8f2c5abb
aguimorefran <aguimorefran@gmail.com>
2020-02-25 18:03:54 +0100

    testing divide and conquer and multiprocessing

---
+++ testMulti.py
@@ -0,0 +1,94 @@
+from classifier import SentimentClassifier
+from concurrent import futures
+from tqdm import tqdm
+from threading import Lock
+import multiprocessing
+
+
+lista = []
+lista.append("Mi hijo es un gángster sin corazón, y yo necesito un abrazo")
+lista.append("¿El tren ya ha partido, queréis alquiler una bicicleta?")
+lista.append("El perro es el mejor amigo del hombre")
+lista.append("Los pingüinos están en el agua")
+lista.append("Un rey no muere nunca, solo duerme.")
+lista.append("Quiero mucho ver las diapositivas de vuestra operación de hígado pero en primer lugar necesito ir cortar mi cabeza en pedazos pequeñitos con mi peine")
+lista.append("Me gustam mucho ir a correr")
+lista.append("Mi amiga es muy guapa")
+lista.append("NO quiero ir a estudiar porque no me gusta")
+lista.append("Mis amigos se portan bien conmigo")
+lista.append("Quiero ir al cine")
+lista.append("Estoy enfadado con mis amigos")
+lista.append("Mi padre ha hecho la comida que me gusta")
+lista.append("No quiero ir al cine")
+lista.append("Me gustam mucho ir a correr")
+lista.append("Mi amiga es muy guapa")
+lista.append("NO quiero ir a estudiar porque no me gusta")
+lista.append("Mis amigos se portan bien conmigo")
+lista.append("Quiero ir al cine")
+lista.append("Estoy enfadado con mis amigos")
+lista.append("Mi padre ha hecho la comida que me gusta")
+lista.append("No quiero ir al cine")
+lista.append("Me gustam mucho ir a correr")
+lista.append("Mi amiga es muy guapa")
+lista.append("NO quiero ir a estudiar porque no me gusta")
+lista.append("Mis amigos se portan bien conmigo")
+lista.append("Quiero ir al cine")
+lista.append("Estoy enfadado con mis amigos")
+lista.append("Mi padre ha hecho la comida que me gusta")
+lista.append("No quiero ir al cine")
+lista.append("Me gustam mucho ir a correr")
+lista.append("Mi amiga es muy guapa")
+lista.append("NO quiero ir a estudiar porque no me gusta")
+lista.append("Mis amigos se portan bien conmigo")
+lista.append("Quiero ir al cine")
+lista.append("Estoy enfadado con mis amigos")
+lista.append("Mi padre ha hecho la comida que me gusta")
+lista.append("No quiero ir al cine")
+lista.append("Me gustam mucho ir a correr")
+lista.append("Mi amiga es muy guapa")
+lista.append("NO quiero ir a estudiar porque no me gusta")
+lista.append("Mis amigos se portan bien conmigo")
+lista.append("Quiero ir al cine")
+lista.append("Estoy enfadado con mis amigos")
+lista.append("Mi padre ha hecho la comida que me gusta")
+lista.append("No quiero ir al cine")
+lista.append("Me gustam mucho ir a correr")
+lista.append("Mi amiga es muy guapa")
+lista.append("NO quiero ir a estudiar porque no me gusta")
+lista.append("Mis amigos se portan bien conmigo")
+lista.append("Quiero ir al cine")
+lista.append("Estoy enfadado con mis amigos")
+lista.append("Mi padre ha hecho la comida que me gusta")
+lista.append("No quiero ir al cine")
+lista.append("Me gustam mucho ir a correr")
+lista.append("Mi amiga es muy guapa")
+lista.append("NO quiero ir a estudiar porque no me gusta")
+lista.append("Mis amigos se portan bien conmigo")
+lista.append("Quiero ir al cine")
+lista.append("Estoy enfadado con mis amigos")
+lista.append("Mi padre ha hecho la comida que me gusta")
+lista.append("No quiero ir al cine")
+
+def classify(msgList):
+    cores = multiprocessing.cpu_count()
+    classifiers = []
+    for c in range(cores):
+        classifiers.append(SentimentClassifier())
+        print(str(c) + " clf initialized")
+    
+    mutex = Lock()
+
+    #divide the list and call the thread with its mod clf
+
+    
+# receives the original list and the bounds it has to compute
+# iterates from msgList[first] to msgList[last], calculates the clf, acquires mutex, and does res[i] = result
+def compute(id, msgList, res, mutex, first, last, clf):
+    for i in range(start=first, stop=last):
+        print(str(id) + " computing " + str(i))
+        result = clf.predict(msgList[i])
+        mutex.acquire()
+        try:
+            res[i] = result
+        finally:
+            mutex.release()
